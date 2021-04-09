d1b315d2fdbc660dcea2f203b2bd7211b09aea65
Diego de Oliveira <42583983+Diegooliver2@users.noreply.github.com>
2021-04-09 18:31:27 -0300

    Primeiro commit

---
+++ Temporizador de pausa/js/script.js
@@ -0,0 +1,64 @@
+
+        let minutos = document.getElementById("minutos")
+        let segundos = document.getElementById("segundos")
+        let tempo = document.getElementById("tempo")
+
+        let segAtual, minAtual, intervalo
+
+        for(let i = 0; i <= 60; i++){
+            minutos.innerHTML += `<option value="${i}">${i}</option>`
+        }
+
+        for(let i = 0; i <= 60; i++){
+            segundos.innerHTML += `<option value="${i}">${i}</option>`
+        }
+
+        tempo.innerHTML = `${minutos.value}:${segundos.value}`
+
+        function cronoP(){
+            clearInterval(intervalo)
+
+            minAtual = minutos.value
+            segAtual = segundos.value
+
+            tempo.innerHTML = `${minAtual}:${segAtual}`
+
+            intervalo = setInterval(function(){
+
+                if(segAtual > 0){
+                    segAtual--
+                }
+
+                if(segAtual <= 0){
+                    if(minAtual > 0){
+                        minAtual--
+                        segAtual = 59
+                    } else {
+                        clearInterval(intervalo)
+                    }
+                }
+
+                tempo.innerHTML = `${minAtual}:${segAtual}`
+            },1000)
+        }
+
+        function pausa10(){
+            minutos.value = 10
+            segundos.value = 0
+
+            cronoP()
+        }
+
+        function pausa20(){
+            minutos.value = 20
+            segundos.value = 0
+
+            cronoP()
+        }
+
+        function zerar(){
+            minutos.value = 0
+            segundos.value = 0
+
+            cronoP()
+        }
